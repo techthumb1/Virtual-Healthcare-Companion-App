a8904e049259fd4472e0f563e7af3ef21d59188c
Jason Robinson <robinsonjason761@gmail.com>
2023-03-25 20:08:33 -0500

    Implement env and middleware

---
+++ backend/config/settings.py
@@ -0,0 +1,46 @@
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env.bool('DEBUG', default=False)
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
+
+INSTALLED_APPS = [
+    'app',
+    'rest_framework',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XContentOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+DATABASES = {
+    'default': env.db()
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
